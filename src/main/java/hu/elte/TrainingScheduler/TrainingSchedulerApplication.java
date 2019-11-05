9c5af7e541bf40916bcf388063b25d9e014ce579
Csató Apor <apor16@gmail.com>
2019-11-05 10:22:28 +0100

    Spring Initializr & database

---
+++ src/main/java/hu/elte/TrainingScheduler/TrainingSchedulerApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.TrainingScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSchedulerApplication.class, args);
+	}
+
+}
